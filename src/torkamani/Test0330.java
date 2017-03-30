59f586d5caaab75aa8b6c0ae95d714394b39216b
sami <sami.torkamani11@yahoo.de>
2017-03-30 13:34:42 +0200

    commit 3 (english)

--- src/torkamani/Test0330.java
+++ src/torkamani/Test0330.java
@@ -4,7 +4,7 @@ public class Test0330 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Guten Tag Hr. Torkamani !!!!");
+		System.out.println("Good Morning Hr. Torkamani !!!!");
 	}
 
 }
